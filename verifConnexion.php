df911c72f7aae5ab56837e6a19a401b510335c62
nicolemoucas <nicolemoucas@gmail.com>
2024-01-04 12:27:14 +0100

    mise à jour redirection

--- verifConnexion.php
+++ verifConnexion.php
@@ -14,7 +14,7 @@
     <script>
         function connexionImpossible() {
                     alert("Votre identifiant ou votre mot de passe est erroné. Veuillez réessayer.");
-                    window.location.href= 'http://localhost/LesAbeilles/NomProvisoire.php';
+                    window.location.href= 'http://localhost/LesAbeilles/index.php';
                 }
         function redirectionProprietaire() {
             window.location.href= 'http://localhost/LesAbeilles/AccueilProprietaire.php';
@@ -30,41 +30,43 @@
 
      </script>
     <body>
-        <?php
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
+        <div class="corps">
+            <?php
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
 
-            $connexion = pg_connect("host=plg-broker.ad.univ-lorraine.fr port=5432 dbname=m1_circuit_nnsh user=connexion_user password=connexion") or die("Impossible de se connecter : " . pg_result_error($connexion));
-            $idUtilisateur = $_POST["identifiant"];
-            $mdpUtilisateur = $_POST["mdp"];
+                $connexion = pg_connect("host=plg-broker.ad.univ-lorraine.fr port=5432 dbname=m1_circuit_nnsh user=connexion_user password=connexion") or die("Impossible de se connecter : " . pg_result_error($connexion));
+                $idUtilisateur = $_POST["identifiant"];
+                $mdpUtilisateur = $_POST["mdp"];
 
-            $verifConnexion = pg_prepare($connexion, "my_verif", 'SELECT verification_utilisateur($1,$2)');
-            $verifConnexion = pg_execute($connexion, "my_verif", array($idUtilisateur, $mdpUtilisateur));
- 
-            $response = pg_fetch_object($verifConnexion);
-            if($response->verification_utilisateur === 't'){
-                $roleEmploye = pg_prepare($connexion, "role_employe", 'SELECT fetch_role_utilisateur($1,$2)');
-                $roleEmploye = pg_execute($connexion, "role_employe", array($idUtilisateur, $mdpUtilisateur));
+                $verifConnexion = pg_prepare($connexion, "my_verif", 'SELECT verification_utilisateur($1,$2)');
+                $verifConnexion = pg_execute($connexion, "my_verif", array($idUtilisateur, $mdpUtilisateur));
+    
+                $response = pg_fetch_object($verifConnexion);
+                if($response->verification_utilisateur === 't'){
+                    $roleEmploye = pg_prepare($connexion, "role_employe", 'SELECT fetch_role_utilisateur($1,$2)');
+                    $roleEmploye = pg_execute($connexion, "role_employe", array($idUtilisateur, $mdpUtilisateur));
 
-                $role = pg_fetch_object($roleEmploye);
-                $_SESSION["identifiant"] = $idUtilisateur;
-                $_SESSION["motdepasse"] = $mdpUtilisateur;
-                $_SESSION["role"]= $role->fetch_role_utilisateur;
+                    $role = pg_fetch_object($roleEmploye);
+                    $_SESSION["identifiant"] = $idUtilisateur;
+                    $_SESSION["motdepasse"] = $mdpUtilisateur;
+                    $_SESSION["role"]= $role->fetch_role_utilisateur;
 
-               switch ($role->fetch_role_utilisateur) {
-                    case 'Propriétaire':
-                        echo '<script type="text/javascript"> redirectionProprietaire(); </script>';
-                        break;
-                    case 'Moniteur':
-                        echo '<script type="text/javascript"> redirectionMoniteur(); </script>';
-                        break;
-                    case 'Garçon de Plage':
-                        echo '<script type="text/javascript"> redirectionGarconPlage(); </script>';
-                        break;
-                } 
-            } else {
-                echo '<script type="text/javascript"> connexionImpossible(); </script>';
-            }
-        ?>
+                switch ($role->fetch_role_utilisateur) {
+                        case 'Propriétaire':
+                            echo '<script type="text/javascript"> redirectionProprietaire(); </script>';
+                            break;
+                        case 'Moniteur':
+                            echo '<script type="text/javascript"> redirectionMoniteur(); </script>';
+                            break;
+                        case 'Garçon de Plage':
+                            echo '<script type="text/javascript"> redirectionGarconPlage(); </script>';
+                            break;
+                    } 
+                } else {
+                    echo '<script type="text/javascript"> connexionImpossible(); </script>';
+                }
+            ?>
+        </div>
     </body>
 </html>
