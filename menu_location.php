5635b0451e4168ffed1ccb5b58573ee9b52005e3
NatsuPow <147069989+NatsuPow@users.noreply.github.com>
2024-01-05 17:21:21 +0100

    Update menu_location.php

--- menu_location.php
+++ menu_location.php
@@ -19,9 +19,19 @@
         <div class="container">
             <h2>Location de Matériel</h2>
             <div class="fonctionnalites">
-            
-            <a href="location.php" class="button">Louer du matériel</a>
+                <a href="#" class="button" onclick="redirectTo('location.php')">Louer Planche à voile</a>
+                <a href="#" class="button" onclick="redirectTo('standuppaddle_disponible.php')">Louer Stand Up Paddle</a>
+                <a href="#" class="button" onclick="redirectTo('location.php')">Louer Catamaran</a>
+                <a href="#" class="button" onclick="redirectTo('location.php')">Louer Pédalo</a>
             </div>
+
+            <script>
+                function redirectTo(page) {
+                    const urlParams = new URLSearchParams(window.location.search);
+                    const idClient = urlParams.get('idClient');
+                    window.location.href = `${page}?idClient=${idClient}`;
+                }
+            </script>
         </div>
     
         <footer>
