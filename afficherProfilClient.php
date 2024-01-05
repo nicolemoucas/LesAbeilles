8cdf121a64ba932f89b376c75907c5aa2824cf1e
NatsuPow <147069989+NatsuPow@users.noreply.github.com>
2024-01-05 17:23:36 +0100

    Update afficherProfilClient.php
    ajout bouton louer materiel

--- afficherProfilClient.php
+++ afficherProfilClient.php
@@ -106,6 +106,7 @@
                 echo '<button class= "button" formaction="javascript:confirmerSuppression()">Supprimer le profil</button>'?>
                 <button class="button" formaction="javascript:confirmerModification()"> Modifier le profil</button>
                 <button class= "button" formaction="javascript:confirmerInscription()">Inscrire à un cours</button>
+                <button class= "button" formaction="javascript:confirmerLocation()">Louer un matériel</button>
         
             </div>
                 <input type="hidden" id="idpers" name="idpers" value="<?php echo $row->idpers; ?>" />
@@ -205,5 +206,14 @@
             document.location = url;
         }
     }
+
+    function confirmerLocation() {
+        const formulaire = document.formulaire;
+        if(confirm("Voulez-vous louer un matériel à ce client ?")) {
+            const url = 'menu_location.php?idClient=' + formulaire.idpers.value;
+            document.location = url;
+        }
+    }
+    
 
 </script>
