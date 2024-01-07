7c710f87c6da655d1069a00e3b147f9385277a68
nicolemoucas <nicolemoucas@gmail.com>
2024-01-07 17:07:47 +0100

    progress

--- header.php
+++ header.php
@@ -8,7 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Les Abeilles</title>
         <link rel="stylesheet" href="css/header.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     </head>
     <script>
         function redirectionPropriétaire() {
