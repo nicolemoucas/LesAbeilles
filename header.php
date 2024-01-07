0bcef39045d5a2bff55bf2ed740267df24f13904
nicolemoucas <nicolemoucas@gmail.com>
2024-01-07 17:33:22 +0100

    correction lien

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="css/header.css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
     </head>
+        <?php $current_url = 'header.php'; ?>
     <script>
         function redirectionPropriétaire() {
                 window.location.href= 'http://localhost/LesAbeilles/AccueilProprietaire.php';
